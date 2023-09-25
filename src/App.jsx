0b7a81227ab72a706729e8e00b08bcd3af10f0d3
JE4NVRG <jean.v1803@gmail.com>
2023-09-25 16:54:17 -0300

    criada todas as secoes do site,

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,30 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world! je4ndev</h1>;
-}
+const App = () => (
+  <main className="relative">
+    Nav
+    <section
+      className="x1:padding-1
+    wide:padding-r padding-b"
+    >
+      Hero
+    </section>
+    <section className="padding">PopularProducts</section>
+    <section className="padding">SuperQuality</section>
+    <section
+      className="padding-x
+    py-10"
+    >
+      Services
+    </section>
+    <section className="padding">EspecialOffer</section>
+    <section
+      className="bg-pale-blue 
+    padding"
+    >
+      CustomerReviews
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+    <section className="bg-black padding-x padding-t pb-8">Footer</section>
+  </main>
+);
+
+export default App;
